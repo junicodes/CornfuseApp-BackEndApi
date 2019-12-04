156eefea9ddd4dc77029c0f2b5305809533c5083
unknown <junicodefire@gmail.com>
2019-12-04 23:07:14 +0100

    working

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 //import express engine
 const express = require('express');
 //Register our Express App
